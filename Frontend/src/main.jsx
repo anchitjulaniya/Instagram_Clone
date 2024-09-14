589da7a2e4a6856ec48783fce2562975d02965dd
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-09-14 09:05:57 +0530

    Initializing repo with frontend and backend

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignUp from './Pages/SignUp.jsx'
+import SignIn from './Pages/SignIn.jsx'
+
+
+const renderFunction = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      }, 
+      {
+        path: '/signup',
+        element: <SignUp />,
+      },
+      {
+        path: '/signin',
+        element: <SignIn />,
+      },
+    ],
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={renderFunction} />
+)
