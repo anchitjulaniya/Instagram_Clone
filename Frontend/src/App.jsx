b0ceb8372ec90b158c66f10ad3d11d05da01bde9
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-09-14 09:23:05 +0530

    adding route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navigate = useNavigate()
 
   return (
     <>
+      
+      <span onClick={() => {navigate('/signup')}}>SiginUp</span>
       <Outlet />
     </>
   )
