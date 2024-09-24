3e3f13b10d24fc9a05eae7d7f283be0c19ca7416
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-09-24 19:55:25 +0530

    updating reel route

--- Frontend/src/Components/BottomNavBar/BottomNavbar.jsx
+++ Frontend/src/Components/BottomNavBar/BottomNavbar.jsx
@@ -11,7 +11,7 @@ function BottomNavbar() {
     <div className="bg-white flex md:hidden fixed bottom-0 items-center justify-evenly w-screen h-[50px] text-2xl border-t border-[#dbdbdb]">
       <span onClick={() => navigate("/")} className="cursor-pointer scale-105 px-3"><IoHomeSharp /></span>
       <span onClick={() => navigate("/search")} className="cursor-pointer scale-105 px-3"><IoSearchSharp /></span>
-      <span onClick={() => navigate("/explore")} className="cursor-pointer scale-105 px-3"><RxVideo /></span>
+      <span onClick={() => navigate("/reels")} className="cursor-pointer scale-105 px-3"><RxVideo /></span>
       <span onClick={() => navigate("/messages")} className="cursor-pointer scale-105 px-3"><TbBrandMessenger /></span>
       <span onClick={() => navigate("/profile")} className="cursor-pointer scale-105 px-3"><CgProfile /></span>
     </div>
